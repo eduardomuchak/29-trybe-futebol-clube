857c6a9da9c258596fd580e51db7afda2333057c
Eduardo Muchak <eduardomuchak@gmail.com>
2022-08-31 16:16:43 -0300

    feat: :sparkles: adjust leaderboard controller to separate complete board

--- app/backend/src/controllers/Leaderboard.controller.ts
+++ app/backend/src/controllers/Leaderboard.controller.ts
@@ -4,21 +4,22 @@ import LeaderboardService from '../services/Leaderboard.service';
 export default class LeaderboardController {
   constructor(private leaderboardService = new LeaderboardService()) {}
 
-  public getLeaderboard = async (req: Request, res: Response) => {
+  public getHomeOrAwayLeaderboard = async (req: Request, res: Response) => {
     const place: string = req.url.split('/')[2];
-
-    if (place === '') {
-      const homeLeaderboard = await this.leaderboardService.getHomeOrAwayLeaderboard('home');
-      const awayLeaderboard = await this.leaderboardService.getHomeOrAwayLeaderboard('away');
-      const leaderboard = this.leaderboardService
-        .getHomeAndAwayLeaderboard(homeLeaderboard, awayLeaderboard);
-      res.status(200).json(leaderboard);
-    } else if (place === 'home') {
+    if (place === 'home') {
       const homeLeaderboard = await this.leaderboardService.getHomeOrAwayLeaderboard('home');
       res.status(200).json(homeLeaderboard);
     } else if (place === 'away') {
       const awayLeaderboard = await this.leaderboardService.getHomeOrAwayLeaderboard('away');
       res.status(200).json(awayLeaderboard);
     }
+  };
+
+  public getCompleteLeaderboard = async (_req: Request, res: Response) => {
+    const homeLeaderboard = await this.leaderboardService.getHomeOrAwayLeaderboard('home');
+    const awayLeaderboard = await this.leaderboardService.getHomeOrAwayLeaderboard('away');
+    const leaderboard = this.leaderboardService
+      .getHomeAndAwayLeaderboard(homeLeaderboard, awayLeaderboard);
+    res.status(200).json(leaderboard);
   };
 }
--- app/backend/src/routes/leaderboard.routes.ts
+++ app/backend/src/routes/leaderboard.routes.ts
@@ -3,8 +3,8 @@ import { leaderboardController } from './main';
 
 const router = Router();
 
-router.get('/leaderboard/home', leaderboardController.getLeaderboard);
-router.get('/leaderboard/away', leaderboardController.getLeaderboard);
-router.get('/leaderboard/', leaderboardController.getLeaderboard);
+router.get('/leaderboard/home', leaderboardController.getHomeOrAwayLeaderboard);
+router.get('/leaderboard/away', leaderboardController.getHomeOrAwayLeaderboard);
+router.get('/leaderboard/', leaderboardController.getCompleteLeaderboard);
 
 export default router;
