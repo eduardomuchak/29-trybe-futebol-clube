37557bc0fbc016584379eb3a9f8b702e994b297c
Eduardo Muchak <eduardomuchak@gmail.com>
2022-09-01 18:43:00 -0300

    test: :test_tube: add matches context tests

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -1,11 +1,16 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
+// import * as jwt from 'jsonwebtoken';
 
 import { app } from '../app';
 import MatchesModel from '../database/models/Matches.model';
+// import UsersModel from '../database/models/Users.model';
+// import UserModel from '../database/models/Users.model';
 
 // @ts-ignore
 import chaiHttp = require('chai-http');
+// import BcryptService from '../services/Bcrypt.service';
+// import JwtService from '../services/Jwt.service';
 
 chai.use(chaiHttp);
 
@@ -89,6 +94,39 @@ const inProgressMatchesMock = [
 	},
 ]
 
+// const successLoginMock = {
+//   email: 'admin@admin.com',
+//   password: 'secret_admin',
+// };
+
+// const userMock = {
+//   id: 1,
+//   username: 'admin',
+//   email: "admin@admin.com",
+//   password: "secret_admin",
+//   role: "admin",
+// }
+
+// const tokenMock = {
+//   token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInBhc3N3b3JkIjoic2VjcmV0X2FkbWluIiwiaWF0IjoxNjYxMTk3NDY1LCJleHAiOjE2NjEyMDEwNjV9.fxwGDtpFSe-Lqt_qD4VWPd9u3sLOsISYIt-0idXIX04"
+// };
+
+const createMatchMock = {
+  homeTeam: 1,
+  awayTeam: 2,
+  homeTeamGoals: 1,
+  awayTeamGoals: 1,
+}
+
+// const createdMatchMock = {
+//   id: 60,
+//   homeTeam: 1,
+//   homeTeamGoals: 1,
+//   awayTeam: 2,
+//   awayTeamGoals: 1,
+//   inProgress: true,
+// }
+
 
 describe('#/matches', () => {
   
@@ -130,4 +168,52 @@ describe('#/matches?inProgress=boolean', () => {
     expect(res.body).to.deep.equal(matchesMock);
   });
 });
+
+describe('#/matches/:id/finish', () => {
+  
+  beforeEach(async () => {
+    sinon.restore();
+  });
+  
+  // it('should return the message "Finished" when a match searched by id is finished', async () => {
+  //   sinon.stub(JwtService, 'verify').returns(tokenMock.token);
+  //   sinon.stub(UsersModel, 'findOne').resolves(userMock as UserModel);
+  //   sinon.stub(BcryptService, 'compareSyncPassword').returns(true);
+  //   sinon.stub(JwtService, 'sign').returns(tokenMock.token);
+  //   sinon.stub(MatchesModel, 'update').resolves();
+
+  //   const loginResponse = await chai.request(app)
+  //     .post('/login')
+  //     .send(successLoginMock);
+
+  //   const res = await chai.request(app).patch('/matches/1/finish');
+
+  //   expect(res.status).to.equal(200);
+  //   expect(res.body).to.deep.equal({ message: 'Finished' }); 
+  // });
+
+  it('should return the message "Authorization not found" when someone try to finish a match without authorization', async () => {
+    sinon.stub(MatchesModel, 'update').resolves();
+
+    const res = await chai.request(app).patch('/matches/1/finish');
+
+    expect(res.status).to.equal(404);
+    expect(res.body).to.deep.equal({ message: 'Authorization not found' }); 
+  });
+});
+
+describe('#POST /matches', () => {
+    
+  beforeEach(async () => {
+    sinon.restore();
+  });
+
+  it('should return the message "Token must be a valid token"', async () => {
+
+    const res = await chai.request(app).post('/matches').send(createMatchMock);
+
+    expect(res.status).to.equal(401);
+    expect(res.body).to.deep.equal({ message: 'Token must be a valid token' }); 
+  });
+})
 
